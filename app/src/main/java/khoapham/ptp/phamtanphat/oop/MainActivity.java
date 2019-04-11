9eb3e2f04279c56d40a741eacd8cf995baba07f1
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-11 20:57:03 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/oop/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/oop/MainActivity.java
@@ -3,6 +3,7 @@ package khoapham.ptp.phamtanphat.oop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,9 +23,19 @@ public class MainActivity extends AppCompatActivity {
 //        nguyenvanb.setTen();
 //        nguyenvanb.setTuoi(25);
 //        nguyenvanb.setPhuongtien("Đi xe đạp");
-        Person nguyenvana = new Person();
-        nguyenvana.ten = " Nguyen Van a";
-        Animal cat = new Animal();
-        cat.ten = "cat";
+//        Person nguyenvana = new Person();
+//        nguyenvana.ten = " Nguyen Van a";
+//        Animal cat = new Animal();
+//        cat.ten = "cat";
+        ShowTen(20);
+    }
+
+    //OVERload : phuong thuc nap chong
+    public void ShowTen(String ten){
+        Toast.makeText(this, ten, Toast.LENGTH_SHORT).show();
+    }
+    public void ShowTen(Integer number){
+        Toast.makeText(this, number + "", Toast.LENGTH_SHORT).show();
+
     }
 }
